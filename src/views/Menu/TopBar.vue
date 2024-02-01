d775bf7726eb346f35876d208ef8abb1f1c14b87
Andre <andre.ncardoso@hotmail.com>
2024-02-01 20:48:53 -0300

    feat: create component top-bar

---
+++ src/views/Menu/TopBar.vue
@@ -0,0 +1,19 @@
+<template>
+    <nav class="navbar sticky-top py-4 px-2">
+        <div class="container-fluid">
+            <div>
+                <a class="navbar-brand" href="#">
+                    <Logo />
+                </a>
+            </div>
+            <div>
+                <button type="button" class="btn btn-outline-success">Entrar</button>
+            </div>
+        </div>
+    </nav>
+    <router-view />
+</template>
+<script setup lang="ts">
+import { Logo } from '@/assets/icons'
+
+</script>
