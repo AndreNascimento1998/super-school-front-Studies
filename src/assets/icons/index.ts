dc0d821daec18ce33aae5444983bfa492d348019
Andre <andre.ncardoso@hotmail.com>
2024-02-01 20:51:23 -0300

    feat: create new index for exports to the icons

---
+++ src/assets/icons/index.ts
@@ -0,0 +1,12 @@
+import Logo from "./logo.vue"
+import Line from './line.vue'
+import Graduation from './graduation.vue'
+import PostGraduation from './post-graduation.vue'
+import TechinicalCourses from './techinical-courses.vue'
+export {
+    Logo,
+    Line,
+    Graduation,
+    PostGraduation,
+    TechinicalCourses
+}
