7cad726d042db34536bb97609cf1ca84a900244f
Andre <andre.ncardoso@hotmail.com>
2024-02-01 20:47:16 -0300

    feat: component carousel

---
+++ src/components/CarouselComponent.vue
@@ -0,0 +1,29 @@
+<!-- src/components/Carousel.vue -->
+<template>
+    <Carousel :items-to-show="1">
+        <Slide v-for="{image} in props.slide" :key="image">
+            <div>
+                <img :src="image">
+            </div>
+        </Slide>
+        <template #addons>
+            <Pagination />
+        </template>
+    </Carousel>
+</template>
+
+<script setup lang="ts">
+import { Carousel, Slide, Pagination } from 'vue3-carousel'
+import 'vue3-carousel/dist/carousel.css'
+
+const props = defineProps({
+    slide:{
+        type: Array,
+        default: [],
+        required: true
+    }
+})
+
+</script>
+
+<style scoped lang="scss"></style>
