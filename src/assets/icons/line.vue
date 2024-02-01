5f2dd79ca26de32d8ab4e648d0b72a9817bddce4
Andre <andre.ncardoso@hotmail.com>
2024-02-01 20:48:26 -0300

    feat: create icons for app

---
+++ src/assets/icons/line.vue
@@ -0,0 +1,6 @@
+<template>
+    <svg width="26" height="2" viewBox="0 0 26 2" fill="none" xmlns="http://www.w3.org/2000/svg">
+        <line y1="1.1" x2="26" y2="1.1" stroke="#00BE4B" stroke-width="0.8"/>
+    </svg>
+</template>
+<script setup lang="ts"></script>
