3722abd97df41f2ded3f3317552d5cc6bb01ee01
Andre <andre.ncardoso@hotmail.com>
2024-02-01 20:49:19 -0300

    feat: create home page view

---
+++ src/views/Home/HomePage.vue
@@ -0,0 +1,6 @@
+<template>
+    <CoursesOverviewPartials />
+</template>
+<script setup lang="ts">
+import CoursesOverviewPartials from "@/views/Home/Partials/CoursesOverviewPartials.vue";
+</script>
