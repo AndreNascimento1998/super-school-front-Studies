e7e3cbf339ccf7f617372d7f3fb5a0e20b43fcca
Andre <andre.ncardoso@hotmail.com>
2024-02-02 16:35:06 -0300

    feat:  add new icons

---
+++ src/assets/icons/registration.vue
@@ -0,0 +1,8 @@
+<template>
+    <svg width="78" height="78" viewBox="0 0 78 78" fill="none" xmlns="http://www.w3.org/2000/svg">
+        <rect width="78" height="78" rx="39" fill="#00BE4B"/>
+        <path d="M39.7222 26H28.1667C27.592 26 27.0409 26.2283 26.6346 26.6346C26.2283 27.0409 26 27.592 26 28.1667V44.0556C26 44.6302 26.2283 45.1813 26.6346 45.5876C27.0409 45.9939 27.592 46.2222 28.1667 46.2222H49.8333C50.408 46.2222 50.9591 45.9939 51.3654 45.5876C51.7717 45.1813 52 44.6302 52 44.0556V39M39 46.2222V52M31.7778 52H46.2222M48.3889 34.6667V26M48.3889 26L44.7778 29.6111M48.3889 26L52 29.6111" stroke="white" stroke-width="2.6" stroke-linecap="round" stroke-linejoin="round"/>
+    </svg>
+</template>
+
+<script setup lang="ts"></script>
---
+++ src/assets/icons/star.vue
@@ -0,0 +1,6 @@
+<template>
+    <svg width="14" height="14" viewBox="0 0 14 14" fill="none" xmlns="http://www.w3.org/2000/svg">
+        <path d="M2.6775 13.6501L3.815 8.7326L0 5.4251L5.04 4.9876L7 0.350098L8.96 4.9876L14 5.4251L10.185 8.7326L11.3225 13.6501L7 11.0426L2.6775 13.6501Z" fill="#D49329"/>
+    </svg>
+</template>
+<script setup lang="ts"></script>
