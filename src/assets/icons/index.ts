1b6bf38a39b23fd43891c0591df8980751d9fb8f
Andre <andre.ncardoso@hotmail.com>
2024-02-02 16:37:16 -0300

    chore: new imports

--- src/assets/icons/index.ts
+++ src/assets/icons/index.ts
@@ -1,6 +1,18 @@
-import Logo from "./logo.vue"
-import Line from './line.vue'
+import LogoIcon from "./logo.vue"
+import LineIcon from './line.vue'
+import FilterIcon from "@/assets/icons/filter.vue";
+import RegisterIcon from '@/assets/icons/register.vue'
+import RegistrationIcon from "@/assets/icons/registration.vue";
+import StudiesIcon from '@/assets/icons/studies.vue'
+import SearchIcon from '@/assets/icons/search.vue'
+import StarIcon from '@/assets/icons/star.vue'
 export {
-    Logo,
-    Line
+    LogoIcon,
+    LineIcon,
+    FilterIcon,
+    RegisterIcon,
+    RegistrationIcon,
+    StudiesIcon,
+    SearchIcon,
+    StarIcon
 }
