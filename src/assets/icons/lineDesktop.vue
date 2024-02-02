1b13ba551037330116157ba92894483912a2ce2b
Andre <andre.ncardoso@hotmail.com>
2024-02-02 20:07:01 -0300

    feat: new icon

---
+++ src/assets/icons/lineDesktop.vue
@@ -0,0 +1,7 @@
+<template>
+    <svg width="802" height="6" viewBox="0 0 802 6" fill="none" xmlns="http://www.w3.org/2000/svg">
+        <path d="M0 0.833496L801.988 5.30158" stroke="#00BE4B" stroke-width="1.10317"/>
+    </svg>
+</template>
+
+<script setup lang="ts"></script>
