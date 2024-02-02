3697ba7c96adc39667bbe981dd0d62fa8992788a
Andre <andre.ncardoso@hotmail.com>
2024-02-02 13:48:15 -0300

    feat: new page for content (about us)

---
+++ src/views/Home/Partials/ContentAboutUs.vue
@@ -0,0 +1,37 @@
+<template>
+    <div>
+        <div>
+            <CardPromotion
+                :content="discountGraduation"
+                width="11rem"
+            />
+        </div>
+    </div>
+</template>
+
+<script setup lang="ts">
+import CardPromotion from "@/components/CardPromotion.vue";
+import {ref, Ref} from "vue";
+import {CardOverview} from "@/model/Interfaces/CardOverview.ts";
+
+const discountGraduation: Ref<Array<CardOverview>> = ref([
+    {
+        id: 1,
+        src: "",
+        title:"Direito",
+        descOpt: "Desconto de ",
+        discount: "20%",
+        desc: "A partir de ",
+        price: "R$ 490,00"
+    },
+    {
+        id: 2,
+        src: '',
+        title:"História",
+        descOpt: "Desconto de ",
+        discount: "80%",
+        desc: "A partir de ",
+        price: "R$ 120,00"
+    }
+])
+</script>
