07cf68f832cf805fdee6c0302db2272c0d786f4a
Andre <andre.ncardoso@hotmail.com>
2024-02-02 13:48:41 -0300

    feat: new component generic card

---
+++ src/components/CardPromotion.vue
@@ -0,0 +1,109 @@
+<template>
+    <div class="d-flex justify-content-around my-4">
+        <div
+            v-for="item in props.content"
+            :key="item.type"
+            @click="handleClick(item.id)"
+            class="card card-modalities align-items-center"
+            :class="{
+                'selected-card': selected && selectedCard === item.id,
+                'py-4': props.paddingY,
+                'cursor-pointer': selected
+            }"
+            :style="{
+                width: props.width,
+                maxHeight: props.maxHeight
+            }"
+        >
+            <img v-if="item.src" class="card-img-top w-50" :src="item.src" alt="Modalidades">
+            <div v-if="item.title" class="card-body ">
+                <p class="card-text text-center card-modalities__title">{{ item.title }}</p>
+            </div>
+            <div class="card-body">
+                <p
+                    v-if="item.descOpt"
+                    style="font-size: 12px"
+                    class="card-text text-center "
+                >
+                    {{ item.descOpt }}
+                    <span
+                        class="card-modalities__discount"
+                    >
+                        {{ item.discount }}
+                    </span>
+                </p>
+                <p style="font-size: 12px" class="card-text text-center ">{{ item.desc }} <span class="card-modalities__price">{{ item.price }}</span> </p>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script setup lang="ts">
+
+import {ref} from "vue";
+
+let selectedCard = ref(1)
+
+const props = defineProps({
+    content: {
+        type: Array<any>,
+        default: [],
+        required: true
+    },
+    width: {
+        type: String,
+        default: "7rem",
+        required: false
+    },
+    maxHeight: {
+        type: String,
+        default: "9rem",
+        required: false
+    },
+    paddingY: {
+        type: Boolean,
+        default: false,
+        required: false
+    },
+    selected: {
+        type: Boolean,
+        default: false,
+        required: false
+    }
+})
+
+const handleClick = (cardId: number) => {
+    selectedCard.value = cardId
+}
+
+</script>
+
+<style scoped lang="scss">
+.cursor-pointer {
+    cursor: pointer;
+}
+.card-modalities {
+    background-color: $background-card;
+    &__title {
+        font-size: 16px;
+        font-weight: 700;
+        color: $primary-color;
+    }
+
+    &__discount {
+        font-size: 14px;
+        font-weight: 700;
+        color: $primary-color;
+    }
+
+    &__price {
+        font-weight: bolder;
+        font-size: 14px;
+    }
+}
+
+.selected-card {
+    border-color: black;
+    border-width: 3px;
+}
+</style>
--- src/views/Home/Partials/CoursesOverviewPartials.vue
+++ src/views/Home/Partials/CoursesOverviewPartials.vue
@@ -6,50 +6,43 @@
         <div class="d-flex mt-4 align-items-center justify-content-center">
             <span class="font-default">Qual é o seu interesse acadêmico?</span>
         </div>
-        <div class="d-flex justify-content-around my-4">
-            <div
-                v-for="image in modalities"
-                :key="image.type"
-                @click="handleClick(image.id)"
-                class="card card-modalities align-items-center py-4"
-                :class="{ 'selected-card': selectedCard === image.id }"
-                style="width: 7.5rem; max-height: 8rem"
-            >
-                <img class="card-img-top w-50" :src="image.type" alt="Modalidades">
-                <div class="card-body">
-                    <p style="font-size: 12px" class="card-text text-center ">{{ image.text }}</p>
-                </div>
-            </div>
+        <div>
+            <CardPromotion
+                :content="modalities"
+                padding-y
+                selected
+            />
         </div>
     </div>
 
 </template>
 <script setup lang="ts">
 import CarouselComponent from "@/components/CarouselComponent.vue";
-import {ref} from "vue";
+import {Ref, ref} from "vue";
 import firstSlide from "@/assets/carousel/firstSlide.png";
 import secondSlide from "@/assets/carousel/secondSlide.png";
 import thirdSlide from "@/assets/carousel/thirdSlide.png";
 import graduation from '@/assets/training modalities/graduation.png'
 import postGraduation from '@/assets/training modalities/post-graduation.png'
 import techinicalCourses from '@/assets/training modalities/techinical-courses.png'
+import CardPromotion from "@/components/CardPromotion.vue";
+import {CardOverview} from "@/model/Interfaces/CardOverview.ts";
 
-let selectedCard = ref(1)
-const modalities = ref([
+const modalities: Ref<Array<CardOverview>> = ref([
     {
         id: 1,
-        type: graduation,
-        text: "Graduação"
+        src: graduation,
+        desc: "Graduação"
     },
     {
         id: 2,
-        type: postGraduation,
-        text: "Pós-graduação"
+        src: postGraduation,
+        desc: "Pós-graduação"
     },
     {
         id: 3,
-        type: techinicalCourses,
-        text: "Cursos técnicos"
+        src: techinicalCourses,
+        desc: "Cursos técnicos"
     }
 ])
 const promotionSlide = ref([
@@ -64,23 +57,12 @@ const promotionSlide = ref([
     }
 ])
 
-const handleClick = (cardId: number) => {
-    selectedCard.value = cardId
-}
 </script>
 
-<style scoped>
-.card-modalities {
-    background-color: #e5f3e8;
-}
-
+<style scoped lang="scss">
 .font-default {
-    color: green;
+    color: $primary-color;
+    font-weight: 700;
     font-size: 20px;
-}
-
-.selected-card {
-    border-color: #00BE4B;
-    border-width: 1px;
 }
 </style>
