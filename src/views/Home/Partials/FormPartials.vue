a0f39e42299f9bd91294debc6b247df64d83538e
Andre <andre.ncardoso@hotmail.com>
2024-02-01 22:32:13 -0300

    feat: new component partials of home page

---
+++ src/views/Home/Partials/FormPartials.vue
@@ -0,0 +1,50 @@
+<template>
+    <div class="px-2">
+        <div>
+            <span>Modalidades:</span>
+            <select class="form-select border-success" aria-label="Default select example">
+                <option
+                    v-for="item in options"
+                    :key="item.value"
+                    :value="item.value"
+                >
+                    {{ item.text }}
+                </option>
+            </select>
+        </div>
+        <div class="my-4">
+            <span>Cursos:</span>
+            <select class="form-select border-success" aria-label="Default select example">
+                <option
+                    v-for="item in options"
+                    :key="item.value"
+                    :value="item.value"
+                >
+                    {{ item.text }}
+                </option>
+            </select>
+        </div>
+        <div class="d-grid">
+            <button class="btn btn-success" type="button">Button</button>
+        </div>
+    </div>
+</template>
+<script setup lang="ts">
+
+import {ref} from "vue";
+
+const options = ref([
+    {
+        text: 'Curso Informática',
+        value: '1'
+    },
+    {
+        text: 'Curso Técnico',
+        value: '2'
+    },
+    {
+        text: 'Curso de Fisiologia',
+        value: '3'
+    }
+])
+</script>
