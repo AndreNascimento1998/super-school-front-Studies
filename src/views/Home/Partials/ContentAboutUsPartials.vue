48c95164d637ed97ca9a02206e1c4020cbfd5896
Andre <andre.ncardoso@hotmail.com>
2024-02-02 20:08:29 -0300

    style: added items for the center

--- src/views/Home/Partials/ContentAboutUsPartials.vue
+++ src/views/Home/Partials/ContentAboutUsPartials.vue
@@ -1,58 +1,63 @@
 <template>
     <div class="content">
-        <div class="container-fluid">
-            <span class="content__font-title">Descontos em cursos de graduação: </span>
-            <p class="font-desc">Modalidade: EAD </p>
-            <CardPromotion
-                :content="discountGraduation"
-                width="11rem"
-            />
-        </div>
-        <div class="my-5 container-fluid">
-            <span class="content__font-title">Descontos em cursos de pós-graduação: </span>
-            <p class="font-desc">Modalidade: EAD </p>
-            <CardPromotion
-                :content="discountGraduation"
-                width="11rem"
-            />
-        </div>
-        <div class="row-cols-2 d-grid mb-5 container-fluid gap-3">
-            <div class="col-12 text-center">
-                <span class="content__title-about-us">Como funciona</span>
+        <div class="row container-fluid">
+            <div class="container-fluid col-12 col-lg-5 col-xl-4">
+                <span class="content__font-title">Descontos em cursos de graduação: </span>
+                <p class="font-desc">Modalidade: EAD </p>
+                <CardPromotion
+                    :content="discountGraduation"
+                    width="11rem"
+                />
             </div>
-            <div class="col-12 d-flex flex-column gap-3 content__modalities">
-                <div class="d-flex align-items-center gap-2">
-                    <SearchIcon />
-                    <span>Busque a modalidade</span>
+            <div class="my-5 my-lg-0 container-fluid col-12 col-lg-5 col-xl-4">
+                <span class="content__font-title">Descontos em cursos de pós-graduação: </span>
+                <p class="font-desc">Modalidade: EAD </p>
+                <CardPromotion
+                    :content="discountGraduation"
+                    width="11rem"
+                />
+            </div>
+            <div class="row-cols-2 d-grid mb-5 container-fluid gap-3">
+                <div class="col-12 text-center">
+                    <span class="content__title-about-us">Como funciona</span>
                 </div>
-                <div class="d-flex align-items-center gap-2">
-                    <FilterIcon />
-                    <span>Filtre o curso</span>
-                </div>
-                <div class="d-flex align-items-center gap-2">
-                    <RegisterIcon />
-                    <span>Realize seu cadastro</span>
-                </div>
-                <div class="d-flex align-items-center gap-2">
-                    <RegistrationIcon />
-                    <span>Conclua sua matrícula</span>
-                </div>
-                <div class="d-flex align-items-center gap-2">
-                    <StudiesIcon />
-                    <span>Inicie seus estudos</span>
+                <div class="col-12 d-flex flex-column justify-content-md-center flex-md-row gap-3 content__modalities">
+                    <div class="d-flex flex-md-column align-items-center gap-2">
+                        <SearchIcon />
+                        <span>Busque a modalidade</span>
+                    </div>
+                    <div class="d-flex flex-md-column  align-items-center gap-2">
+                        <FilterIcon />
+                        <span>Filtre o curso</span>
+                    </div>
+                    <div class="d-flex flex-md-column  align-items-center gap-2">
+                        <RegisterIcon />
+                        <span>Realize seu cadastro</span>
+                    </div>
+                    <div class="d-flex flex-md-column  align-items-center gap-2">
+                        <RegistrationIcon />
+                        <span>Conclua sua matrícula</span>
+                    </div>
+                    <div class="d-flex flex-md-column  align-items-center gap-2">
+                        <StudiesIcon />
+                        <span>Inicie seus estudos</span>
+                    </div>
                 </div>
             </div>
         </div>
         <div class="content__more-about-us">
             <div class="container-fluid py-4 d-flex flex-column gap-4">
-                <div class="content__title-more-about-us">
+                <div class="content__title-more-about-us d-flex">
                     Sobre nós
                 </div>
                 <span v-html="aboutUs" class="content__desc-more-about-us" />
-                <img :src="imageAboutUs" alt="Sobre nós">
+                <div class="d-md-flex justify-content-md-center">
+                    <img :src="imageAboutUs" class="content__img" alt="Sobre nós">
+                </div>
             </div>
         </div>
-        <div>
+        <div class="pt-4 text-center">
+            <span class="content__title-testemonial">DEPOIMENTOS DOS ALUNOS</span>
             <CardTestimonials :content="userTestemonial" />
         </div>
     </div>
@@ -71,7 +76,6 @@ import {CardTestemonial} from "@/model/Interfaces/CardTestemonial.ts";
 const discountGraduation: Ref<Array<CardOverview>> = ref([
     {
         id: 1,
-        src: "",
         title:"Direito",
         descOpt: "Desconto de ",
         discount: "20%",
@@ -80,7 +84,22 @@ const discountGraduation: Ref<Array<CardOverview>> = ref([
     },
     {
         id: 2,
-        src: '',
+        title:"História",
+        descOpt: "Desconto de ",
+        discount: "80%",
+        desc: "A partir de ",
+        price: "R$ 120,00"
+    },
+    {
+        id: 3,
+        title:"Direito",
+        descOpt: "Desconto de ",
+        discount: "20%",
+        desc: "A partir de ",
+        price: "R$ 490,00"
+    },
+    {
+        id: 4,
         title:"História",
         descOpt: "Desconto de ",
         discount: "80%",
@@ -97,7 +116,23 @@ const userTestemonial: Ref<Array<CardTestemonial>> = ref([
         rating: 5,
         title: "Serviço impecável ",
         desc: "Minha experiência superou todas as expectativas. Cursos de alta qualidade e atendimento excepcional. "
-    }
+    },
+    {
+        id: 2,
+        src: imageTestemonial,
+        name: "Maria Conceição",
+        rating: 5,
+        title: "Serviço impecável ",
+        desc: "Minha experiência superou todas as expectativas. Cursos de alta qualidade e atendimento excepcional. "
+    },
+    {
+        id: 3,
+        src: imageTestemonial,
+        name: "Maria Conceição",
+        rating: 5,
+        title: "Serviço impecável ",
+        desc: "Minha experiência superou todas as expectativas. Cursos de alta qualidade e atendimento excepcional. "
+    },
 ])
 
 </script>
@@ -136,6 +171,26 @@ const userTestemonial: Ref<Array<CardTestemonial>> = ref([
         font-size: 16px;
         font-style: normal;
         font-weight: 400;
+    }
+    &__img {
+        max-width: 400px;
+    }
+
+    &__title-testemonial {
+        color: $primary-color;
+        text-align: justify;
+        font-size: 24px;
+        font-style: normal;
+        font-weight: 700;
+    }
+}
+
+@media (max-width: 1024px) {
+    .content {
+
+        &__modalities {
+            font-size: 14px;
+        }
     }
 }
 </style>
--- src/views/Home/Partials/CoursesOverviewPartials.vue
+++ src/views/Home/Partials/CoursesOverviewPartials.vue
@@ -1,17 +1,19 @@
 <template>
     <div class="container-fluid">
-        <div>
-            <CarouselComponent :slide="promotionSlide" />
-        </div>
-        <div class="d-flex mt-4 align-items-center justify-content-center">
-            <span class="font-default">Qual é o seu interesse acadêmico?</span>
-        </div>
-        <div>
-            <CardPromotion
-                :content="modalities"
-                padding-y
-                selected
-            />
+            <div class="d-sm-none">
+                <CarouselComponent :slide="promotionSlide" />
+            </div>
+            <div>
+            <div class="d-flex mt-4 align-items-center justify-content-center">
+                <span class="font-default">Qual é o seu interesse acadêmico?</span>
+            </div>
+            <div>
+                <CardPromotion
+                    :content="modalities"
+                    padding-y
+                    selected
+                />
+            </div>
         </div>
     </div>
 
@@ -43,7 +45,22 @@ const modalities: Ref<Array<CardOverview>> = ref([
         id: 3,
         src: techinicalCourses,
         desc: "Cursos técnicos"
-    }
+    },
+    {
+        id: 4,
+        src: techinicalCourses,
+        desc: "Cursos técnicos"
+    },
+    {
+        id: 5,
+        src: techinicalCourses,
+        desc: "Cursos técnicos"
+    },
+    {
+        id: 6,
+        src: techinicalCourses,
+        desc: "Cursos técnicos"
+    },
 ])
 const promotionSlide = ref([
     {
