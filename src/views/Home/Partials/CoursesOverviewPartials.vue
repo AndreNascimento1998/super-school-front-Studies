34c554def3e92d5153f8159923ff49f4b3d78cb7
Andre <andre.ncardoso@hotmail.com>
2024-02-01 22:31:44 -0300

    refactor: added cards select in icons

--- src/views/Home/Partials/CoursesOverviewPartials.vue
+++ src/views/Home/Partials/CoursesOverviewPartials.vue
@@ -7,9 +7,19 @@
             <span class="font-default">Qual é o seu interesse acadêmico?</span>
         </div>
         <div class="d-flex justify-content-around my-4">
-            <Graduation />
-            <PostGraduation />
-            <TechinicalCourses />
+            <div
+                v-for="image in modalities"
+                :key="image.type"
+                @click="handleClick(image.id)"
+                class="card card-modalities"
+                :class="{ 'selected-card': selectedCard === image.id }"
+                style="width: 7.5rem"
+            >
+                <img class="card-img-top" :src="image.type" alt="Modalidades">
+                <div class="card-body">
+                    <p style="font-size: 12px" class="card-text text-center ">{{ image.text }}</p>
+                </div>
+            </div>
         </div>
     </div>
 
@@ -20,8 +30,28 @@ import {ref} from "vue";
 import firstSlide from "@/assets/carousel/firstSlide.png";
 import secondSlide from "@/assets/carousel/secondSlide.png";
 import thirdSlide from "@/assets/carousel/thirdSlide.png";
-import {Graduation, PostGraduation, TechinicalCourses} from "@/assets/icons";
+import graduation from '@/assets/training modalities/graduation.png'
+import postGraduation from '@/assets/training modalities/post-graduation.png'
+import techinicalCourses from '@/assets/training modalities/techinical-courses.png'
 
+let selectedCard = ref(1)
+const modalities = ref([
+    {
+        id: 1,
+        type: graduation,
+        text: "Graduação"
+    },
+    {
+        id: 2,
+        type: postGraduation,
+        text: "Pós-graduação"
+    },
+    {
+        id: 3,
+        type: techinicalCourses,
+        text: "Cursos técnicos"
+    }
+])
 const promotionSlide = ref([
     {
         image: firstSlide
@@ -33,10 +63,24 @@ const promotionSlide = ref([
         image: thirdSlide
     }
 ])
+
+const handleClick = (cardId: number) => {
+    selectedCard.value = cardId
+}
 </script>
+
 <style scoped>
+.card-modalities {
+    background-color: #e5f3e8;
+}
+
 .font-default {
     color: green;
     font-size: 20px;
+}
+
+.selected-card {
+    border-color: #00BE4B;
+    border-width: 1px;
 }
 </style>
