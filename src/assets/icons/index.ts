754a71a1a647c3cfd6a01d1f13590be95ee1f0a0
Andre <andre.ncardoso@hotmail.com>
2024-02-02 17:58:37 -0300

    chore: added new imports

--- src/assets/icons/index.ts
+++ src/assets/icons/index.ts
@@ -6,6 +6,12 @@ import RegistrationIcon from "@/assets/icons/registration.vue";
 import StudiesIcon from '@/assets/icons/studies.vue'
 import SearchIcon from '@/assets/icons/search.vue'
 import StarIcon from '@/assets/icons/star.vue'
+import BrazilFlag from "@/assets/icons/BrazilFlag.vue";
+import InstagramIcon from "@/assets/icons/instagram.vue";
+import FacebookIcon from "@/assets/icons/facebook.vue";
+import YoutubeIcon from "@/assets/icons/youtube.vue";
+import LinkedinIcon from "@/assets/icons/linkedin.vue";
+import LineSeparatorIcon from "@/assets/icons/lineSeparator.vue";
 export {
     LogoIcon,
     LineIcon,
@@ -14,5 +20,11 @@ export {
     RegistrationIcon,
     StudiesIcon,
     SearchIcon,
-    StarIcon
+    StarIcon,
+    BrazilFlag,
+    InstagramIcon,
+    FacebookIcon,
+    YoutubeIcon,
+    LinkedinIcon,
+    LineSeparatorIcon
 }
--- src/views/Home/HomePage.vue
+++ src/views/Home/HomePage.vue
@@ -1,10 +1,10 @@
 <template>
     <CoursesOverviewPartials />
     <FormPartials />
-    <ContentAboutUs class="mt-5" />
+    <ContentAboutUsPartials class="mt-5" />
 </template>
 <script setup lang="ts">
 import CoursesOverviewPartials from "@/views/Home/Partials/CoursesOverviewPartials.vue";
 import FormPartials from "@/views/Home/Partials/FormPartials.vue";
-import ContentAboutUs from "@/views/Home/Partials/ContentAboutUs.vue";
+import ContentAboutUsPartials from "@/views/Home/Partials/ContentAboutUsPartials.vue";
 </script>
