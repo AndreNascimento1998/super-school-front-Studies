9fed6e7e787a534328df3ea19cb5625e557ae8f3
Andre <andre.ncardoso@hotmail.com>
2024-02-01 22:33:21 -0300

    fix: svg replaced by images

--- src/assets/icons/index.ts
+++ src/assets/icons/index.ts
@@ -1,12 +1,6 @@
 import Logo from "./logo.vue"
 import Line from './line.vue'
-import Graduation from './graduation.vue'
-import PostGraduation from './post-graduation.vue'
-import TechinicalCourses from './techinical-courses.vue'
 export {
     Logo,
-    Line,
-    Graduation,
-    PostGraduation,
-    TechinicalCourses
+    Line
 }
