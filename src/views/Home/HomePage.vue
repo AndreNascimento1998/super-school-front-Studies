ba057a6909078ffd79a6819ff0cb8671da597010
Andre <andre.ncardoso@hotmail.com>
2024-02-01 22:32:32 -0300

    chore: added component

--- src/views/Home/HomePage.vue
+++ src/views/Home/HomePage.vue
@@ -1,6 +1,8 @@
 <template>
     <CoursesOverviewPartials />
+    <FormPartials />
 </template>
 <script setup lang="ts">
 import CoursesOverviewPartials from "@/views/Home/Partials/CoursesOverviewPartials.vue";
+import FormPartials from "@/views/Home/Partials/FormPartials.vue";
 </script>
