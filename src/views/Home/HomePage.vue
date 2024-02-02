1ae176ace77c5e2a6768cd8c49eb5cbeaeb36cb8
Andre <andre.ncardoso@hotmail.com>
2024-02-02 13:49:22 -0300

    chore: new partials

--- src/views/Home/HomePage.vue
+++ src/views/Home/HomePage.vue
@@ -1,8 +1,10 @@
 <template>
     <CoursesOverviewPartials />
     <FormPartials />
+    <ContentAboutUs />
 </template>
 <script setup lang="ts">
 import CoursesOverviewPartials from "@/views/Home/Partials/CoursesOverviewPartials.vue";
 import FormPartials from "@/views/Home/Partials/FormPartials.vue";
+import ContentAboutUs from "@/views/Home/Partials/ContentAboutUs.vue";
 </script>
