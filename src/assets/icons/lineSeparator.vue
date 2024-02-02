3df69eb05691dec2881453fbb3db469c1210fe00
Andre <andre.ncardoso@hotmail.com>
2024-02-02 17:55:49 -0300

    feat: new icons

---
+++ src/assets/icons/lineSeparator.vue
@@ -0,0 +1,7 @@
+<template>
+    <svg width="331" height="4" viewBox="0 0 331 4" fill="none" xmlns="http://www.w3.org/2000/svg">
+        <path d="M0 1L331 2.84408" stroke="#00BE4B" stroke-width="1.10317"/>
+    </svg>
+</template>
+
+<script setup lang="ts"></script>
