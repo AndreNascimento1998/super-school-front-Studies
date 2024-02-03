e5291ea6d7450e6a8b5efd56bbdbf22b0a3000e8
Andre <andre.ncardoso@hotmail.com>
2024-02-03 03:09:55 -0300

    feat: store global for the checkout to persist the data

---
+++ src/stores/CheckoutStore.ts
@@ -0,0 +1,25 @@
+import { defineStore } from 'pinia'
+import {Ref, ref} from 'vue'
+
+export const useCheckoutStore = defineStore('checkoutStore', () => {
+    const name: Ref<string> = ref('')
+    const email: Ref<string> = ref('')
+    const phone: Ref<string> = ref('')
+    const cep: Ref<string> = ref('')
+    const cpf: Ref<string> = ref('')
+    const dateBirth: Ref<string> = ref('')
+    const fileCnh: Ref<File> = ref(null)
+    const fileRg: Ref<File> = ref(null)
+
+    return {
+        name,
+        email,
+        phone,
+        cep,
+        cpf,
+        dateBirth,
+        fileCnh,
+        fileRg
+    }
+})
+
