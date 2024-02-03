d1b671215f23c4c756f1f5bec5bb762cac82b4f3
Andre <andre.ncardoso@hotmail.com>
2024-02-03 03:10:39 -0300

    feat: new component generic, for input select

---
+++ src/components/InputSelect.vue
@@ -0,0 +1,40 @@
+<template>
+    <div>
+        <span style="font-weight: bold">{{ props.label }}:</span>
+        <select v-model="value" @change="changeSelect" class="form-select border-success" aria-label="Default select example">
+            <option
+                v-for="item in props.items"
+                :key="item.value"
+                :value="item"
+            >
+                {{ item.text }}
+            </option>
+        </select>
+    </div>
+</template>
+
+<script setup lang="ts">
+import {ref} from "vue";
+
+const emits = defineEmits(
+    ['update:modelValue']
+)
+const value = ref(null)
+
+const props = defineProps({
+    items: {
+        type: Array<any>,
+        default: [],
+        required: true
+    },
+    label: {
+        type: String,
+        default: '',
+        required: true
+    }
+})
+
+function changeSelect() {
+    emits('update:modelValue', value.value)
+}
+</script>
