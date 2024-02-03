4922d034c2518fb5794955f20cc7684bbd3813e1
Andre <andre.ncardoso@hotmail.com>
2024-02-03 03:10:51 -0300

    feat: new component generic, for input default

---
+++ src/components/InputText.vue
@@ -0,0 +1,37 @@
+<template>
+    <div>
+        <div class="input-group input-group-sm mb-3">
+            <div class="input-group-prepend">
+                <span class="input-group-text" id="inputGroup-sizing-sm">{{ props.label }}</span>
+            </div>
+            <input v-model="value" @input="handleInput" type="text" class="form-control" :placeholder="props.placeholder"  aria-label="Small" aria-describedby="inputGroup-sizing-sm">
+        </div>
+    </div>
+</template>
+<script setup lang="ts">
+
+import {ref} from "vue";
+
+const emits = defineEmits(
+    ['update:modelValue']
+)
+
+const value = ref('')
+
+const props = defineProps({
+    label: {
+        type: String,
+        default: "",
+        required: true
+    },
+    placeholder: {
+        type: String,
+        default: "",
+        required: false
+    }
+})
+
+function handleInput() {
+    emits('update:modelValue', value.value)
+}
+</script>
