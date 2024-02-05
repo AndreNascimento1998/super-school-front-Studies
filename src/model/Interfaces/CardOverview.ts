d63962c1d5d648ba654fd02cb16715d9a87bd5fb
Andre <andre.ncardoso@hotmail.com>
2024-02-05 19:09:32 -0300

    refactor: type CardOverview for graduation

--- src/model/Interfaces/CardOverview.ts
+++ src/model/Interfaces/CardOverview.ts
@@ -1,9 +1,5 @@
 export interface CardOverview {
     id: number,
     src?: string,
-    title?: string,
-    descOpt?: string,
-    discount?: string,
-    price?: string,
-    desc: string,
+    title: string,
 }
