d90b3a2b846b03d9e177801ed4b08be4d115d303
Andre <andre.ncardoso@hotmail.com>
2024-02-05 22:06:09 -0300

    refactor: component generic, format for default design system

--- src/components/CardPromotion.vue
+++ src/components/CardPromotion.vue
@@ -1,24 +1,46 @@
 <template>
     <div class="d-flex my-4 flex-wrap gap-2">
         <div
-            @click="() => handleClick(content)"
-            class="card card-modalities align-items-center"
+            class="card  card-modalities p-4 align-items-center"
             :class="{
                 'd-none': !content.title,
                 'py-4': props.paddingY,
                 'cursor-pointer': cursorPointer,
-
+                'rounded-4': props.rounded
             }"
         >
-            <img v-if="content.src" class="card-img-top w-50" :src="content.src" alt="Modalidades">
-            <div v-if="content.title" class="card-body ">
-                <p class="card-text card-modalities__title">{{ content.title }}</p>
+            <div v-if="typeGraduation" class="card-body py-0 mb-4 ">
+                <div class="card-modalities__title">
+                    Tipo: {{typeGraduation}}
+                </div>
             </div>
-
-            <div v-if="content.price" class="card-body d-flex flex-column gap-2">
-                <div class="card-modalities__discount">Clique aqui e adquira agora!</div>
-                <div v-if="content.discount " class="card-modalities__text">Desconto de <span class="card-modalities__discount">{{ content.discount }}%</span></div>
-                <div class="card-modalities__text">A partir de <span class="card-modalities__discount">R${{ content.price }}</span></div>
+            <div class="card-modalities__subtitle">{{ content.title }}</div>
+            <div class="card-modalities__text-generic">
+                a partir
+            </div>
+            <div
+                :class="{
+               'card-modalities__letter': priceDiscounted,
+               'card-modalities__price': !priceDiscounted
+            }"
+            >
+                {{ globalStore.formatCurrency(content?.price) }}
+            </div>
+            <div class="card-modalities__price" v-if="priceDiscounted">
+                {{ globalStore.formatCurrency(priceDiscounted) }}
+            </div>
+            <div class="mt-4 mb-1">
+                <div v-if="description" v-for="item in description" :key="description.id">
+                    <div class="d-flex align-items-center px-5 gap-2">
+                        <span class="card-modalities__ball-li"></span>
+                        <div class="card-modalities__text-generic">{{ item.text }}</div>
+                    </div>
+                </div>
+            </div>
+            <div class="d-grid py-2 col-12 h-md-25 align-self-md-end">
+                <button @click="handleClick(content)" class="btn btn-success" type="button">
+                    ADQUIRA JÁ ESTE CURSO
+                </button>
             </div>
         </div>
     </div>
@@ -26,9 +48,11 @@
 
 <script setup lang="ts">
 
-import {ref} from "vue";
+import {computed, ref} from "vue";
 import {CardOverview} from "@/model/Interfaces/CardOverview.ts";
+import {useGlobalStore} from "@/stores/GlobalStore.ts";
 
+const globalStore = useGlobalStore()
 let selectedCard = ref(1)
 
 const emits = defineEmits(
@@ -65,7 +89,29 @@ const props = defineProps({
         type: Boolean,
         default: false,
         required: false
+    },
+    typeGraduation: {
+        type: String,
+        default: '',
+        required: false
+    },
+    description: {
+        type: Array<Object>,
+        default: [],
+        required: false
+    },
+    rounded: {
+        type: Boolean,
+        default: false,
+        required: false
     }
+})
+
+const priceDiscounted = computed(() => {
+    if (props.content?.discount) {
+        return globalStore.discountPrice(props.content.price, props.content.discount)
+    }
+    return null
 })
 
 function handleClick (item: CardOverview) {
@@ -76,44 +122,91 @@ function handleClick (item: CardOverview) {
 </script>
 
 <style scoped lang="scss">
-.cursor-pointer {
-    cursor: pointer;
-}
 .card-modalities {
-    width: 7rem;
-    max-width: 9rem;
-    background-color: $background-card;
     &__title {
+        color: #404040;
+        font-size: 20px;
+        font-style: normal;
+        font-weight: 600;
+    }
+
+    &__subtitle {
+        color: $primary-color;
         font-size: 16px;
-        font-weight: 700;
-        color: $primary-color;
-    }
-
-    &__text {
-        font-size: 12px;
-    }
-
-    &__discount {
-        font-size: 14px;
-        font-weight: 700;
-        color: $primary-color;
+        font-style: normal;
+        font-weight: 600;
     }
 
     &__price {
-        font-weight: bolder;
-        font-size: 14px;
+        color: $primary-color;
+        font-size: 18px;
+        font-style: normal;
+        font-weight: 600;
     }
-}
-//
-//.selected-card {
-//    border-color: black;
-//    border-width: 2px;
-//}
 
-@media (min-width: 1024px) {
-    .card-modalities {
-        width: 10rem;
-        max-width: 12rem;
+    &__text-generic {
+        color: #404040;
+        font-size: 14px;
+        font-style: normal;
+        font-weight: 400;
+    }
+
+    &__letter {
+        font-size: 14px;
+        font-style: italic;
+        color: black;
+        font-weight: bolder;
+        text-decoration: line-through;
+    }
+
+    &__ball-li {
+        width: 4px;
+        height: 4px;
+        background-color: $primary-color;
+        border-radius: 50%;
+        display: inline-block;
     }
 }
+
+
+//.cursor-pointer {
+//    cursor: pointer;
+//}
+//.card-modalities {
+//    //width: 7rem;
+//    //max-width: 9rem;
+//    background-color: $background-card;
+//    &__title {
+//        font-size: 16px;
+//        font-weight: 700;
+//        color: $primary-color;
+//    }
+//
+//    &__text {
+//        font-size: 12px;
+//    }
+//
+//    &__discount {
+//        font-size: 14px;
+//        font-weight: 700;
+//        color: $primary-color;
+//    }
+//
+//    &__price {
+//        font-weight: bolder;
+//        font-size: 14px;
+//    }
+//}
+////
+////.selected-card {
+////    border-color: black;
+////    border-width: 2px;
+////}
+//
+//@media (min-width: 1024px) {
+//    .card-modalities {
+//        //width: 10rem;
+//        //max-width: 12rem;
+//    }
+//}
 </style>
