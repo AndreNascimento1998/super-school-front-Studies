5fbdd116b70b9a0c9824653e0f913fda52221364
Andre <andre.ncardoso@hotmail.com>
2024-02-05 22:06:32 -0300

    feat: functions format currency and calculate discount

--- src/stores/GlobalStore.ts
+++ src/stores/GlobalStore.ts
@@ -4,8 +4,24 @@ import {Ref, ref} from 'vue'
 export const useGlobalStore = defineStore('globalStore', () => {
     const userName: Ref<string>  = ref('André')
 
+    function formatCurrency(value: number): string {
+        const options = {
+            style: 'currency',
+            currency: 'BRL',
+            minimumFractionDigits: 2,
+        }
+        return value.toLocaleString('pt-Br', options)
+    }
+
+    function discountPrice(price: number, discount: number) {
+        const discountFloat = discount / 100
+        return  price - (price * discountFloat)
+    }
+
     return {
         userName,
+        formatCurrency,
+        discountPrice
     }
 })
 
