8ad0145958ad2f48c93deb4719b5c6286c1b7ccb
Andre <andre.ncardoso@hotmail.com>
2024-02-06 19:08:26 -0300

    chore: added buttons for back to home

--- src/views/Checkout/Partials/CourseDescriptionPartials.vue
+++ src/views/Checkout/Partials/CourseDescriptionPartials.vue
@@ -6,7 +6,7 @@
             </div>
             <div class="d-flex flex-column gap-4">
                 <div class="d-flex flex-column gap-2">
-                    <span class="matter-title">{{ courseDataStore.overviewDataCourse.title }}</span>
+                    <span class="matter-title"> {{ courseDataStore.overviewDataCourse.name }}</span>
                     <div>
                         <span class="content-title">{{ courseDataStore.overviewDataCourse.typeModality }}- </span><span class="text-description">{{ courseDataStore.overviewDataCourse.period }}</span>
                     </div>
@@ -20,28 +20,45 @@
                     </div>
                 </div>
             </div>
+            <div v-if="courseDataStore.overviewDataCourse.maxInstallments != 1" class="d-flex justify-content-end">
+                <InputSelect v-model="courseDataStore.overviewDataCourse.installmentChoose" label="Parcelas" installments :items="installmets" />
+            </div>
             <LineSeparator class="d-flex d-md-none" />
             <div>
                 <div class="d-flex justify-content-between">
                     <span class="content-title">Número máximo de parcelas: </span><span class="content-description">{{ courseDataStore.overviewDataCourse.maxInstallments }}x</span>
                 </div>
-                <LineDesktop class="d-none d-md-flex" />
-                <div v-if="courseDataStore.overviewDataCourse.maxInstallments" class="d-flex justify-content-between">
-                    <span class="content-title">Em {{ courseDataStore.overviewDataCourse.maxInstallments }}x</span><span class="content-description">R$ {{ courseDataStore.overviewDataCourse.price / courseDataStore.overviewDataCourse.maxInstallments }}</span>
-                </div>
-                <LineDesktop class="d-none d-md-flex" />
                 <div class="d-flex justify-content-between">
+                    <span class="content-title">Em {{ courseDataStore.overviewDataCourse.installmentChoose.name }}x</span><span class="content-description">{{ globalStore.formatCurrency(priceDiscount / courseDataStore.overviewDataCourse.installmentChoose.name) }}</span>
+                </div>
+                <div v-if="courseDataStore.overviewDataCourse.discount" class="d-flex justify-content-between">
                     <span class="content-title">Desconto: </span><span class="content-description">{{ courseDataStore.overviewDataCourse.discount }}%</span>
                 </div>
-                <LineDesktop class="d-none d-md-flex" />
                 <div class="d-flex justify-content-between">
-                    <span class="content-title">Você pagará mensalmente:  </span><span class="content-description">R$ {{ courseDataStore.overviewDataCourse.price }}</span>
+                    <span class="content-title">Você pagará mensalmente:  </span>
+                    <span class="content-description text-end">
+                        <span
+                            v-if="courseDataStore.overviewDataCourse.discount"
+                            class="value-discount"
+                        >
+                            {{globalStore.formatCurrency(courseDataStore.overviewDataCourse.price)}}
+                        </span>
+                        por {{ globalStore.formatCurrency(priceDiscount) }}
+                    </span>
                 </div>
             </div>
-            <div class="d-grid col-12 col-md-4 h-md-25 align-self-md-end">
-                <button @click="handleClick" class="btn btn-success" type="button">
-                    Avançar
-                </button>
+
+            <div class="d-grid d-md-flex flex-md-row-reverse  gap-3">
+                <div class="d-grid col-12 col-md-4 h-md-25 align-self-md-end">
+                    <button @click="handleClick" class="btn btn-success" type="button">
+                        Avançar
+                    </button>
+                </div>
+                <div class="d-grid col-12 col-md-4 h-md-25 align-self-md-end">
+                    <button @click="handleClickReturn" class="btn btn-success" type="button">
+                        Voltar
+                    </button>
+                </div>
             </div>
         </div>
     </div>
@@ -51,12 +68,38 @@
 import LineSeparator from "@/assets/icons/lineSeparator.vue";
 import LineDesktop from "@/assets/icons/lineDesktop.vue";
 import {useCourseDataStore} from "@/stores/CourseDataStore.ts";
+import {computed, onMounted} from "vue";
+import {useGlobalStore} from "@/stores/GlobalStore.ts";
+import router from "@/router";
+import InputSelect from "@/components/Input/InputSelect.vue";
 
 const courseDataStore = useCourseDataStore()
+const globalStore = useGlobalStore()
 
 const emits = defineEmits(
     ['click-event']
 )
+
+const priceDiscount = computed(() => {
+    if (courseDataStore.overviewDataCourse.discount){
+        return  globalStore.discountPrice(courseDataStore.overviewDataCourse.price, courseDataStore.overviewDataCourse.discount)
+    }
+    return courseDataStore.overviewDataCourse.price
+})
+
+const installmets = computed(() => {
+    const installmentArray = []
+    for (let c = 0; c < courseDataStore.overviewDataCourse.maxInstallments; c++){
+         let installment = { name: c + 1 };
+         installmentArray.push(installment)
+    }
+
+    return installmentArray
+})
+
+function handleClickReturn() {
+    router.push({name: 'home'})
+}
 
 function handleClick() {
     emits('click-event', 1)
@@ -97,5 +140,11 @@ function handleClick() {
     font-size: 14px;
     font-style: normal;
     font-weight: 600;
+}
+
+.value-discount {
+    font-style: italic;
+    font-weight: bold;
+    text-decoration: line-through;
 }
 </style>
