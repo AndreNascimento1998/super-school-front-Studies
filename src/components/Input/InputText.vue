88b645dc434cc596158f7ce0c08d11396f06fb3d
Andre <andre.ncardoso@hotmail.com>
2024-02-07 00:04:31 -0300

    chore: create validation fields

--- src/components/Input/InputText.vue
+++ src/components/Input/InputText.vue
@@ -4,42 +4,101 @@
             <div class="input-group-prepend">
                 <span class="input-group-text" id="inputGroup-sizing-sm">{{ props.label }}</span>
             </div>
-            <input v-model="value" @input="handleInput" type="text" class="form-control" :placeholder="props.placeholder"  aria-label="Small" aria-describedby="inputGroup-sizing-sm">
+            <input
+                v-model="value"
+                @input="handleInput"
+                :class="{ 'is-invalid': isInvalid }"
+                type="text"
+                class="form-control"
+                :placeholder="props.placeholder"
+                aria-label="Small"
+                aria-describedby="inputGroup-sizing-sm"
+            />
+            <div v-if="isInvalid" class="invalid-feedback position-absolute error ">
+                {{ errorMessage }}
+            </div>
         </div>
     </div>
 </template>
+
 <script setup lang="ts">
+import {ref, watch, defineProps, defineEmits, computed} from 'vue';
 
-import {ref, watch} from "vue";
-
-const emits = defineEmits(
-    ['update:modelValue']
-)
+const emits = defineEmits(['update:modelValue']);
 
 const props = defineProps({
     label: {
         type: String,
-        default: "",
-        required: true
+        default: '',
+        required: true,
     },
     placeholder: {
         type: String,
-        default: "",
-        required: false
+        default: '',
+        required: false,
     },
     modelValue: {
         type: String,
-        default: ""
-    }
-})
-
-const value = ref(props.modelValue)
-
-watch(() => props.modelValue, (newValue) => {
-    value.value = newValue;
+        default: '',
+    },
+    required: {
+        type: Boolean,
+        default: false,
+        required: false
+    },
+    minCaracteres: {
+        type: Number,
+        default: 0,
+        required: false
+    },
 });
 
+const value = ref(props.modelValue);
+const isInvalid = ref(false);
+
+watch(
+    () => props.modelValue,
+    (newValue) => {
+        value.value = newValue;
+        validate();
+    }
+);
+
 function handleInput() {
-    emits('update:modelValue', value.value)
+    validate();
+    emits('update:modelValue', value.value);
 }
+
+function validate() {
+    if (props.required) {
+        isInvalid.value = value.value.trim() === '';
+    } else if (props.minCaracteres > 0) {
+        isInvalid.value = value.value.length < props.minCaracteres;
+    } else {
+        isInvalid.value = false;
+    }
+}
+
+const errorMessage = computed(() => {
+    if (props.required && isInvalid.value) {
+        return 'Campo obrigatório.';
+    } else if (props.minCaracteres > 0 && isInvalid.value) {
+        return `Mínimo de ${props.minCaracteres} caracteres.`;
+    } else {
+        return '';
+    }
+});
 </script>
+
+<style scoped lang="scss">
+.error {
+    top: 32px;
+}
+
+.is-invalid {
+    border-color: #dc3545 !important;
+}
+.invalid-feedback {
+    color: #dc3545;
+}
+</style>
